bc2f7ce7163118e539ce049d7be7d008d45fcedc
RafaelSvgh <rafaplaying@gmail.com>
2025-06-24 23:12:23 -0400

    refactor

--- lib/src/router/go_router.dart
+++ lib/src/router/go_router.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sw1/src/pages/home_page.dart';
-import 'package:flutter_sw1/src/pages/incident_page.dart';
 import 'package:flutter_sw1/src/pages/splash_page.dart';
 import 'package:go_router/go_router.dart';
 
-// final goRouter = GoRouter(
-//   initialLocation: '/splash',
-//   errorBuilder: (_, __) => const Scaffold(body: Center(child: Text('Error'))),
-//   routes: [
-//     GoRoute(path: '/splash', builder: (context, state) => const SplashPage()),
-//     GoRoute(path: '/home1', builder: (context, state) => const HomePage()),
-//     GoRoute(path: '/prueba', builder: (context, state) => const Pruebaa()),
-//   ],
-// );
 GoRouter createGoRouter(GlobalKey<NavigatorState> navigatorKey) {
   return GoRouter(
     navigatorKey: navigatorKey,
